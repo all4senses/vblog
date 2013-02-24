c6f51165767749673381aa447485c5ee784eefc3
all4senses <all4senses@gmail.com>
2013-02-24 20:53:08 +0200

    Replace body texts

--- themes/vb/templates/node--article.tpl.php
+++ themes/vb/templates/node--article.tpl.php
@@ -59,7 +59,20 @@
               if (!empty($node->field_extra_data['und'][0]['value'])) {
                 $extra_data = unserialize($node->field_extra_data['und'][0]['value']);
                 //dpm($extra_data);
-                $extra_data['guest_author'] = $author_name = !empty($extra_data['guest_author']) ? $extra_data['guest_author'] : NULL;
+                //$extra_data['guest_author'] = $author_name = !empty($extra_data['guest_author']) ? $extra_data['guest_author'] : NULL;
+                
+                if (!empty($extra_data['guest_author'])) {
+                  if (strpos($extra_data['guest_author'], '; ')) {
+                    $extra_data['guest_author'] = explode('; ', $extra_data['guest_author']);
+                    $extra_data['guest_author'] = $author_name = l($extra_data['guest_author'][0], $extra_data['guest_author'][1], array('absolute' => TRUE));
+                  }
+                  else {
+                    $extra_data['guest_author'] = $author_name = $extra_data['guest_author'];
+                  }
+                }
+                else {
+                  $extra_data['guest_author'] = $author_name = NULL;
+                }
               }
 
               if (!$extra_data['guest_author']) {
