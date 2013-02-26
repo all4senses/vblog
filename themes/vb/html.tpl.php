f7063bc29f306eb59f06151be2819ee1548b9a13
all4senses <all4senses@gmail.com>
2013-02-26 23:44:23 +0200

    Logo font

--- themes/vb/html.tpl.php
+++ themes/vb/html.tpl.php
@@ -6,6 +6,7 @@
 <head>
   <title><?php global $altered_head_title; if ($altered_head_title) {print $altered_head_title;} else {print $head_title;} ?></title>
   <?php print $head; ?>
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Tangerine">
   <!-- Set the viewport width to device width for mobile -->
   <meta name="viewport" content="width=device-width" />
   <?php print $styles; ?>
