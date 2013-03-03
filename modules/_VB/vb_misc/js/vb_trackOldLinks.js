1657bb3b4a79a4b0757f941092f499d9fcaecd5b
all4senses <all4senses@gmail.com>
2013-03-03 18:49:08 +0200

    Old track url func

--- modules/_VB/vb_misc/js/vb_trackOldLinks.js
+++ modules/_VB/vb_misc/js/vb_trackOldLinks.js
@@ -1,15 +1,17 @@
+function tracking(url){
+  console.log('track - ' + url);
+}
+       
 (function ($) {
 
   Drupal.behaviors.vb_trackOldLinks = {
     attach: function (context, settings) {
-       
-       //$(".yt-direct").colorbox({iframe:true, innerWidth:425, innerHeight:344});
 
        tracking('xxx');
        
-       function tracking(url){
-         console.log('track - ' + url);
-       }
+//       function tracking(url){
+//         console.log('track - ' + url);
+//       }
        
     }
   };
