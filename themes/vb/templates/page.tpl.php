7540663cdc2bc935e3be1d6ceba248d133e10642
all4senses <all4senses@gmail.com>
2013-04-13 20:28:24 +0300

    Home page styling

--- themes/vb/templates/page.tpl.php
+++ themes/vb/templates/page.tpl.php
@@ -153,20 +153,21 @@
   <footer id="footer" role="contentinfo" class="clearfix">
     <div id="footer-inside">
 
-      <!--<div id="in-touch"></div>-->
-
+      
+      <div class="c">Copyright © 2005-2013 voip-weblog.com</div>
+      
       <?php 
         //echo render($page['footer']);
         
         //echo vb_blocks_getBlockThemed(array('module' => 'vb_blocks', 'delta' => 'send_msg_n_subscribe', 'no_subject' => TRUE/*, 'class' => 'block-om-maximenu'*/, 'shadow' => FALSE)/*, TRUE, '+31 day'*/ /*, ($user->uid ? '_logged' : NULL)*/);
         
-//        echo vb_blocks_getBlockThemed(array('module' => 'vb_blocks', 'delta' => 'social_links', 'no_subject' => TRUE/*, 'class' => 'block-om-maximenu'*/, 'shadow' => FALSE)/*, TRUE, '+31 day'*/ /*, ($user->uid ? '_logged' : NULL)*/);
+        echo vb_blocks_getBlockThemed(array('module' => 'vb_blocks', 'delta' => 'social_links', 'no_subject' => TRUE/*, 'class' => 'block-om-maximenu'*/, 'shadow' => FALSE)/*, TRUE, '+31 day'*/ /*, ($user->uid ? '_logged' : NULL)*/);
 //        echo vb_blocks_getBlockThemed(array('module' => 'vb_blocks', 'delta' => 'footer_links', 'no_subject' => TRUE/*, 'class' => 'block-om-maximenu'*/, 'shadow' => FALSE)/*, TRUE, '+31 day'*/ /*, ($user->uid ? '_logged' : NULL)*/);
         
         
       ?>
       
-      <div class="c">Copyright © 2005-2013 voip-weblog.com</div>
+      
     </div>
   </footer> <!-- /#footer -->
 
